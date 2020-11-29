ba1d7d98288b82fcade7c066d9a1edb31963309d
Aaron Lee <aaronlab.net@gmail.com>
2020-11-29 14:29:44 +0900

    feat: Project Setup

---
+++ MovieSearch/Loaders/ImageLoader.swift
@@ -0,0 +1,34 @@
+//
+//  ImageLoader.swift
+//  MovieSearch
+//
+//  Created by Aaron Lee on 2020-11-29.
+//
+
+import Foundation
+
+class ImageLoader: ObservableObject {
+    
+    @Published var downloadedData: Data?
+    
+    func downloadImage(url: String) {
+        
+        guard let imageURL = URL(string: url) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: imageURL) { data, _, error in
+            
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.downloadedData = data
+            }
+            
+        }.resume()
+        
+    }
+    
+}
---
+++ MovieSearch/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MovieSearch
+//
+//  Created by Aaron Lee on 2020-11-29.
+//
+
+import Foundation
+
+struct Constants {
+    
+}
