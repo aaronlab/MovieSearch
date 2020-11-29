cefc4ec2f80aa880f849ba939b32931adafdd7a4
Aaron Lee <aaronlab.net@gmail.com>
2020-11-29 14:36:44 +0900

    feat: Implement Movie Model

---
+++ MovieSearch/Models/Movie.swift
@@ -0,0 +1,30 @@
+//
+//  Movie.swift
+//  MovieSearch
+//
+//  Created by Aaron Lee on 2020-11-29.
+//
+
+import Foundation
+
+struct MovieResponse: Codable {
+    let movies: [Movie]
+    
+    private enum CodingKeys: String, CodingKey {
+        case movies = "Search"
+    }
+}
+
+struct Movie: Codable {
+    let title: String
+    let year: String
+    let imdbId: String
+    let poster: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "Title"
+        case year = "Year"
+        case imdbId = "imdbID"
+        case poster = "Poster"
+    }
+}
