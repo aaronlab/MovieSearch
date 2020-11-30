e3f6ff7a09df42acbb53d95e396379f47a9153c7
Aaron Lee <aaronlab.net@gmail.com>
2020-11-30 18:40:42 +0900

    Remove unncessary model properties

--- MovieSearch/Models/MovieDetail.swift
+++ MovieSearch/Models/MovieDetail.swift
@@ -7,58 +7,28 @@
 
 import Foundation
 
-struct MovieDetail: Codable {
+struct MovieDetail: Decodable {
     
     let title: String
     let year: String
     let rated: String
-    let released: String
-    let runtime: String
-    let genre: String
-    let director: String
-    let writer: String
-    let actors: String
     let plot: String
-    let language: String
-    let country: String
-    let awards: String
-    let poster: String
-    let metascore: String
+    let director: String
+    let actors: String
     let imdbRating: String
-    let imdbVotes: String
+    let poster: String
     let imdbId: String
-    let type: String
-    let dvd: String
-    let boxOffice: String
-    let production: String
-    let website: String
-    let response: String
     
     private enum CodingKeys: String, CodingKey {
         case title = "Title"
         case year = "Year"
         case rated = "Rated"
-        case released = "Released"
-        case runtime = "Runtime"
-        case genre = "Genre"
-        case director = "Director"
-        case writer = "Writer"
-        case actors = "Actors"
         case plot = "Plot"
-        case language = "Language"
-        case country = "Country"
-        case awards = "Awards"
-        case poster = "Poster"
-        case metascore = "Metascore"
+        case director = "Director"
+        case actors = "Actors"
         case imdbRating = "imdbRating"
-        case imdbVotes = "imdbVotes"
+        case poster = "Poster"
         case imdbId = "imdbID"
-        case type = "Type"
-        case dvd = "DVD"
-        case boxOffice = "BoxOffice"
-        case production = "Production"
-        case website = "Website"
-        case response = "Response"
     }
     
 }
